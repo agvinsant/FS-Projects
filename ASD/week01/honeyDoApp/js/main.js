5e848f4a7b92566c64dc943542552b61d3c50efc
Adam Vinsant <agvinsant@fullsail.edu>
2012-09-27 23:31:17 -0400

    Refactoring, refactoring, refactoring, still no fuctions fire at all! Doesn't even give me errors… URG!!! Well here it is for week 1

--- ASD/week01/honeyDoApp/js/main.js
+++ ASD/week01/honeyDoApp/js/main.js
@@ -9,18 +9,22 @@ $('#home').on('pageinit', function(){
         
 $('#addItem').on('pageinit', function(){
 
-    /*    var myForm = $('#choreForm');  //Code commented out because using my own validate function
-            myForm.validate({
+       var myForm = $('#choreForm');  //Code commented out because using my own validate function
+            delete $.validator.methods.date;
+			myForm.validate({
             invalidHandler: function(form, validator) {
             },
             submitHandler: function() {
         var data = myForm.serializeArray();
             storeData(this.key);
         }
-    });*/
+    });
     // moved these variables here to fix scope issues
     var    errMsg = $('#errors'),  
         choreForm = $('#choreForm');
+		
+	var opt= ["Select Chore Type", "Inside", "Outside", "Errand", "Make Phone Call", "Pay Bill"],  //For New App...Change this to fit to new HTML
+        urgencyValue;	
     
     // Getting the value of the radio input types    
     var getSelectedRadio = function() {
@@ -30,56 +34,11 @@ $('#addItem').on('pageinit', function(){
                var urgencyValue = radios[i].val();
             };
         };
+		console.log('radio function works');
     };
     
    
     
-    // Deletes individual items in list
-    var deleteItem = function (){
-        var ask = confirm("Are you sure you want to delete this chore?");     
-            if(ask){
-                localStorage.removeItem(this.key);
-                window.location.reload();
-            }else{
-                alert("Chore was not deleted!");    
-                window.location.reload();
-                return false;
-            }            
-    };
-                                                    
-    var clearLocal = function(){
-        if(localStorage.length === 0) {
-            alert('There is no data to clear.');
-        } else {
-            localStorage.clear();
-            alert('All chores are deleted.');
-            window.location.reload();
-            return false;
-        }
-    };
-      
-      var makeItemLinks = function(key, linksLi) {
-        //add edit single item link
-        var editLink = $('a');
-        editLink.href = '#additem';
-        editLink.key = key;
-        var editText = "Edit Chore";        
-        editLink.attr("class", "editLink");
-        editLink.on('click', editItem);
-        editLink.html(editText);
-        linksLi.append(editLink);
-        
-        // delete Link
-        var deleteLink = $('a');
-        deleteLink.href = "#";
-        deleteLink.key = key;
-        var deleteText = "Delete Chore";        
-        deleteLink.attr("class", "deleteLink");
-        deleteLink.on('click', deleteItem);
-        deleteLink.html(deleteText);
-        linksLi.append(deleteLink);
-        
-    };
     
     //Saving Data to Local Storage
     var storeData = function(key){
@@ -87,7 +46,7 @@ $('#addItem').on('pageinit', function(){
             var id = Math.floor(Math.random()*10000001);
         }else{
             id = key;
-        }
+        };
         getSelectedRadio();
         var item= {};
             item.choretype = ['Chore Type:', $('#choretype').val()];
@@ -100,39 +59,8 @@ $('#addItem').on('pageinit', function(){
             
         localStorage.setItem(id, JSON.stringify(item));
         alert("Chore Saved");
-    };
-    
-    // Getting Data from Local Storage and Displaying in a new page        
-    var getData = function(){
-        // tells function if the data is empty then you will be alerted and will revert back to form with display button missing
-        if(localStorage.length === 0) {
-            alert('There are no chores at this time so default data was added.');        
-       
-            autoFillData(); //delete this for working model. uncomment toggleControls function above.
-        }
-        
-        $('<div id="items"></div>').appendTo('#showList');
-        $('<ul class="chorelist"></ul>').appendTo('#items');
-        
-        for (var i=0, len=localStorage.length; i<len; i++) {
-                $('<li class="eachchore"></li>').appendTo('.chorelist');
-                var linksLi = $('<li></li>');
-                var key = localStorage.key(i);
-                var value = localStorage.getItem(key);
-                
-                // Convert string from local storage into object
-                var object = JSON.parse(value);
-                var makeSubList = $('<ul id="each"></ul>');
-                makeSubList.appendTo('.eachchore');
-                getImage(object.choretype[1], makeSubList);
-                for(var n in object) {
-                    var makeSubLi = $('<li></li>');
-                    var optSubText = object[n][0] + " " +object[n][1];
-                    makeSubLi.appendTo('#each');
-                    linksLi.appendTo('#each');    
-                };
-            makeItemLinks(localStorage.key(i), linksLi); // Create the edit and delete buttons/links for each item in local storage
-        };
+		
+		console.log('storeData works');
     };
     
     //Get image for catagory being displayed
@@ -144,13 +72,25 @@ $('#addItem').on('pageinit', function(){
             var newImg = $('img')
                 .attr("src", "images/" + typeName + ".jpg")
                 .append("#imageLi" + key);
-    
+				
+				console.log('getImage works');
     };
     
-    var validate = function(e){
-            var getChoreType = $('#choretype').val();    //change var's and id's to fit form type
-            var getChoreName = $('#chorename').val();
-            var getFinishBy  = $('#finishby').val();
+    // fills local storage with JSON Data
+    var autoFillData = function (){
+            for(var n in json){
+                var id = Math.floor(Math.random()*100000001);
+                localStorage.setItem(id,JSON.stringify(json[n]));
+				
+            };
+			console.log('autofillData works');
+    };
+	
+	//Validate function
+/*    var validate = function(e){
+            var getChoreType = $('#choretype');    //change var's and id's to fit form type
+            var getChoreName = $('#chorename');
+            var getFinishBy  = $('#finishby');
             
             //reset error messages
             $(".error").hide();
@@ -165,25 +105,25 @@ $('#addItem').on('pageinit', function(){
             var messageArray = [];
             
             //Chore Type validation
-            if(getChoreType.value === "Select Chore Type") {        //Change to fit form type
+            if(getChoreType.val("Select Chore Type")){        //Change to fit form type
                 var typeError = "!!!Please choose a chore type!!!";
                 getChoreType.css("border","3px solid red");
                 messageArray.push(typeError);
-            }
+            };
             
             //Chore Name Validation
-            if(getChoreName.value === "") {
+            if(getChoreName.val("")){
                 var choreNameError = "!!!Please enter a chore name!!!";        //Change to fit form type
                 getChoreName.css("border","3px solid red");
                 messageArray.push(choreNameError);
-            }
+            };
             
             //Finish By Validation
-            if(getFinishBy.value === "") {
+            if(getFinishBy.val("")){
                 var finishByError = "!!!Please enter a finish date!!!";        //Change to fit form type
                 getFinishBy.css("border","3px solid red");
                 messageArray.push(finishByError);
-            }
+            };
             
             // Display errors if any on the screen.
             if(messageArray.length >=1) {
@@ -191,17 +131,17 @@ $('#addItem').on('pageinit', function(){
                     var text = document.createElement('li');
                     text.innerHTML = messageArray[i];
                     errMsg.appendChild(text);
-                }
+                };
             e.preventDefault();
             return false;
             }else{
                 storeData(this.key);
-            }
-            
-      };
+            };
+          console.log('validate function works');   
+      };*/
       
-	  // edit individual chores
-      var editItem = function() {
+	
+	var editItem = function() {
         // Getting data from local storage
         var value = localStorage.getItem(this.key);
         var item = JSON.parse(value);
@@ -225,35 +165,122 @@ $('#addItem').on('pageinit', function(){
         
         var editSubmit = $('#submitButton');
         //remove the initial listener from the input submitButton
-        editSubmit.unbind("click");;
+        $(editSubmit).off("click");
         //change submitButton value to Edit button
-        editSubmit.val("Edit Chore");        //change to fit form type
+        $(editSubmit).val("Edit Chore");        //change to fit form type
         
         // save key value established in this function as a property of the editSubmit event
         // so we can use that value when we save the data we edited.
-        editSubmit.on("click", validate);
-        editSubmit.key = this.key;
+        $(editSubmit).on("click", storeData);
+        $(editSubmit).key = this.key;
+		console.log('editItem works');
+    };
+   
+   
+    // Deletes individual items in list
+    var deleteItem = function (){
+        var ask = confirm("Are you sure you want to delete this chore?");     
+            if(ask){
+                localStorage.removeItem(this.key);
+                window.location.reload();
+            }else{
+                alert("Chore was not deleted!");    
+                window.location.reload();
+                return false;
+            } 
+			console.log('deleteItem works');           
+    };
+	
+	// Make edit and delete links for each chore
+      var makeItemLinks = function(key, linksLi) {
+        //add edit single item link
+        var editLink = $('a');
+        $(editLink).attr("href","#additem");
+        $(editLink).key = key;
+        var editText = "Edit Chore";        
+        $(editLink).addClass("editLink")
+        	.on('click', editItem)
+        	.html(editText);
+        $(linksLi).append(editLink);
+        
+        // delete Link
+        var deleteLink = $('a');
+        $(deleteLink).attr("href","#");
+        $(deleteLink).key = key;
+        var deleteText = "Delete Chore";        
+        $(deleteLink).addClass("deleteLink")
+        	.on('click', deleteItem)
+        	.html(deleteText);
+        $(linksLi).append(deleteLink);
+		
+		console.log('makeItemLinks works');
+        
     };
     
-    // fills local storage with JSON Data
-    var autofillData = function (){
-            for(var n in json){
-                var id = Math.floor(Math.random()*100000001);
-                localStorage.setItem(id,JSON.stringify(json[n]));
-            }
+    // Getting Data from Local Storage and Displaying in a new page        
+    var getData = function(){
+        // tells function if the data is empty then you will be alerted and will revert back to form with display button missing
+        if(localStorage.length === 0) {
+            alert('There are no chores at this time so default data was added.');        
+       
+            autoFillData(); //delete this for working model. uncomment toggleControls function above.
+        }
+        
+        var makeDiv = $('<div id="items"></div>');
+        $(makeDiv).appendTo('#showList');
+        var makeList = $('ul');
+        $(makeList).addClass("chorelist")
+        	.appendTo('#items');
+        
+        for (var i=0, len=localStorage.length; i<len; i++) {
+                var eachChore = $('li');
+                $(eachChore).addClass('eachChore')
+               		.appendTo('.chorelist');
+                var linksLi = $('<li></li>');
+                var key = localStorage.key(i);
+                var value = localStorage.getItem(key);
+                
+                // Convert string from local storage into object
+                var object = JSON.parse(value);
+                var makeSubList = $('<ul id="each"></ul>');
+                $(makeSubList).appendTo('.eachchore');
+                getImage(object.choretype[1], makeSubList);
+                for(var n in object) {
+                    var makeSubLi = $('<li></li>');
+                    var optSubText = object[n][0] + " " +object[n][1];
+                    $(makeSubLi).appendTo('#each');
+                    $(linksLi).appendTo('#each');    
+                };
+            makeItemLinks(localStorage.key(i), linksLi); // Create the edit and delete buttons/links for each item in local storage
+        };
+		console.log('getData works');
     };
+   
+    //Clear Local Storage                                                
+    var clearLocal = function(){
+        if(localStorage.length === 0) {
+            alert('There is no data to clear.');
+        } else {
+            localStorage.clear();
+            alert('All chores are deleted.');
+            window.location.reload();
+            return false;
+        }
+		console.log('clearLocal works');
+    };
+      
+      
     
     // Global Variables
-        urgencyValue;
    
     var displayButton = $('#displayButton'),
         clearButton = $('#clearButton'),
         submitButton = $('#submitButton');
         
         //Event Handlers
-        clearButton.on("click", clearLocal);
-        submitButton.on("click", validate);
-        displayButton.on("click", getData);
+        $(clearButton).on("click", clearLocal);
+        $(submitButton).on("click", storeData);
+        $(displayButton).on("click", getData);
         
     
     
@@ -272,6 +299,13 @@ $('#construction').on('pageinit', function(){
 $('#about').on('pageinit', function(){
     //code needed for home page goes here
 });    
+
+
+
+
+
+
+
 
 
 
