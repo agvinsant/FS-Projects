b7adfc28be4dc7d79da45ae0592fe0796d417e89
Adam Vinsant <agvinsant@fullsail.edu>
2012-11-26 23:42:21 -0500

    commiting android start

---
+++ AVF/appsAndroid/assets/www/js/main.js
@@ -0,0 +1,10 @@
+
+
+$(document).on(function() {
+	    
+	    $('a[href=#top]').click(function(){
+	        $('html, body').animate({scrollTop:0}, 'slow');
+	        return false;
+	    });
+	 
+	});
